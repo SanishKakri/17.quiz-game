77415ab6002ed9c1cee378018fff24b6128cc66c
Sanish Kakri <Sanishkarkii3@gmail.com>
2024-12-06 12:51:50 +0545

    ...

---
+++ main.js
@@ -0,0 +1,11 @@
+const answerButton = document.querySelector("answers");
+const nextButton = document.querySelector("nextBtn");
+const questionElement = document.querySelector("questions");
+
+async function startQuiz() {}
+
+function checkNext() {
+  document.getElementById("nxtBtn").style.display = "block";
+}
+
+function nextQuestion() {}
